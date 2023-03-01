8cf35e020c466e86f5a739b8cdb60fd93622169a
Garrett <90536997+garrett-huggins@users.noreply.github.com>
2023-03-01 13:17:06 -0500

    udpate resume

--- components/Resume/ResumeHeader.tsx
+++ components/Resume/ResumeHeader.tsx
@@ -3,7 +3,7 @@ export default function ResumeHeader() {
     <div className="mb-10 flex justify-between flex-wrap max-w-4xl mx-auto items-end">
       <p className="sm:text-5xl text-4xl">Garrett Huggins</p>
 
-      <p className="sm:text-3xl text-2xl text-gray-400">Frontend Developer</p>
+      <p className="sm:text-3xl text-2xl text-gray-400">Fullstack Developer</p>
     </div>
   );
 }
